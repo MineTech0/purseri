cdb2675795c3878c416a2e46194bcf1716a86d37
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-01-28 02:06:26 +0200

    added result type

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,18 @@
-
 export interface RecordFormData {
-  name: string;
-  reason: string;
-  date: Date;
-  info: string;
+  name: string
+  reason: string
+  date: Date
+  info: string
 }
 export interface RecordFormDataWithShip extends RecordFormData {
-  shipId: string;
+  shipId: string
 }
 export interface ShipFormData {
-  name: string;
-  owner: string;
-  shipmaster: string;
+  name: string
+  owner: string
+  shipmaster: string
+}
+export interface FormResult {
+  type: 'success' | 'error'
+  message: string
 }
