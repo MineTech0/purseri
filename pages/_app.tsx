43b428f49318c212e329c117eaf4275e9fa2f070
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-01-28 02:09:50 +0200

    added auth check for dashboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,49 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, signIn, useSession } from 'next-auth/react'
+import React from 'react'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+const Auth: React.FC<{}> = ({ children }) => {
+  const { data: session, status } = useSession()
+  const isUser = !!session?.user
+  React.useEffect(() => {
+    if (status == 'loading') return // Do nothing while loading
+    if (!isUser) signIn() // If not authenticated, force log in
+  }, [isUser, status])
+
+  if (isUser) {
+    return <>{children}</>
+  }
+
+  // Session is being fetched, or no user.
+  // If no user, useEffect() will redirect.
+  return <div>Loading...</div>
+}
+
+const RESTRICTED_PATHS = ['/dashboard']
+function MyApp({
+  Component,
+  router: { route },
+  pageProps: {
+    session,
+    ...pageProps
+  },
+}: AppProps) {
+  const requireAuth = RESTRICTED_PATHS.some((path) => route.startsWith(path))
   return (
     <SessionProvider session={session}>
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
+      {requireAuth ? (
+        <Auth>
+          <NextUIProvider>
+            <Component {...pageProps} />
+          </NextUIProvider>
+        </Auth>
+      ) : (
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      )}
     </SessionProvider>
   )
 }
