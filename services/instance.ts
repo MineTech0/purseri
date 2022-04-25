e8f132598da4673b4219f1ee39de667c20f48808
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-04-25 23:40:28 +0300

    api address fix

--- services/instance.ts
+++ services/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NEXTAUTH_URL,
+  baseURL: process.env.NEXTAUTH_URL + '/api',
   headers: {
     "Content-type": "application/json"
   }
