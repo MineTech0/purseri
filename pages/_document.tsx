7f34eb30420c12fd5bc5e746cea0be22536d0390
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-04-26 17:52:44 +0300

    added icons and manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,36 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { CssBaseline } from '@nextui-org/react'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx)
     return {
       ...initialProps,
-      styles: <>{initialProps.styles}</>
-    };
+      styles: <>{initialProps.styles}</>,
+    }
   }
 
   render() {
     return (
       <Html>
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          {CssBaseline.flush()}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-status-bar" content="#0070f3" />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
