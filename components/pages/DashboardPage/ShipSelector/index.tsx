1bc693a5665c1b341ad73da465fd7ba732369cc1
Niilo Kurki <niilo.kurki@hotmail.fi>
2022-05-21 16:43:17 +0300

    added overflow control and formating

--- components/pages/DashboardPage/ShipSelector/index.tsx
+++ components/pages/DashboardPage/ShipSelector/index.tsx
@@ -1,32 +1,32 @@
-import { Button } from '@nextui-org/react';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { Ship } from '../../../../lib/db/entity/Ship';
-import AddShipButton from './AddShipButton';
-import ShipButton from './ShipButton';
+import { Button } from '@nextui-org/react'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { Ship } from '../../../../lib/db/entity/Ship'
+import AddShipButton from './AddShipButton'
+import ShipButton from './ShipButton'
 
 interface Props {
-  ships: Ship[];
+  ships: Ship[]
   selectShipHandler: (id: string) => void
 }
 
-const ShipSelector = ({ships, selectShipHandler} : Props): JSX.Element => {
+const ShipSelector = ({ ships, selectShipHandler }: Props): JSX.Element => {
   const router = useRouter()
-    const addShipHandler = () => {
-      router.push('dashboard/ship/new')
-    }
+  const addShipHandler = () => {
+    router.push('dashboard/ship/new')
+  }
 
-    if(ships.length === 0){
-        return (
-            <AddShipButton fullWidth={true} addShip={addShipHandler}/>
-        )
-    }
+  if (ships.length === 0) {
+    return <AddShipButton fullWidth={true} addShip={addShipHandler} />
+  }
   return (
-    <Button.Group>
-        {ships.map((ship) => (<ShipButton key={ship.id} ship={ship} selectShip={selectShipHandler}/>))}
-        <AddShipButton addShip={addShipHandler}/>
+    <Button.Group css={{ overflowX: 'auto'}}>
+      {ships.map((ship) => (
+        <ShipButton key={ship.id} ship={ship} selectShip={selectShipHandler} />
+      ))}
+      <AddShipButton addShip={addShipHandler} />
     </Button.Group>
-  );
-};
+  )
+}
 
-export default ShipSelector;
+export default ShipSelector
